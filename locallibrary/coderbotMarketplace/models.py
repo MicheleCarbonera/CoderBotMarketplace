233e9a04fd3a059b1ef9d5272487eb99fe3bece0
MicheleCarbonera <49145321+MicheleCarbonera@users.noreply.github.com>
2020-05-27 12:11:16 +0200

    begin develop webapp

--- locallibrary/coderbotMarketplace/models.py
+++ locallibrary/coderbotMarketplace/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class package_db(models.Model):
+    NamePackage = models.CharField(max_length=100)
+    IT_Name = models.CharField(max_length=100)
+    IT_Desc = models.CharField(max_length=500)
+    image_cover = models.CharField(max_length=500)
--- locallibrary/coderbotMarketplace/views.py
+++ locallibrary/coderbotMarketplace/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from coderbotMarketplace.models import package_db
 
 # Create your views here.
+def index(request):
+    res = package_db.objects.all()
+    context = {"packages": res}
+    
+    return render(request, "index.html",context)
